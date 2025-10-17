ae512b1e0186a25e55ddd16a6cedac9e847920fc
Jules Alviar <jules.alviar@gmail.com>
2025-10-17 15:38:22 +0800

    Add FOR_IMPLEMENTATION to AipStatus enum

--- src/aip/aip-status.enum.ts
+++ src/aip/aip-status.enum.ts
@@ -1,6 +1,7 @@
 export enum AipStatus {
   CREATED = 'Created',
   ONGOING = 'Ongoing',
+  FOR_IMPLEMENTATION = 'For Implementation',
   COMPLETED = 'Completed',
   INCOMPLETE = 'Incomplete',
   UNIMPLEMENTED = 'Unimplemented',
